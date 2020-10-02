65a918291496f474eb95132b5038e6796fcbcbbe
Yinusa <yamusa2@uic.edu>
2020-10-02 10:59:45 -0500

    second part done

--- client/src/components/PlantList.js
+++ client/src/components/PlantList.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import axios from "axios";
+//import axios from "axios";
 
 export default class PlantList extends Component {
   // add state with a property called "plants" - initialize as an empty array
